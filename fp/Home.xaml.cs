30aace4c09e4dd0070fee4a52f03c32685eba25d
浩 于 <2480444683@qq.com>
2018-05-04 09:52:28 +0800

    加入home界面的Loaded事件，使得每次跳转回home界面时能显示出标题或者内容不为空的笔记

--- fp/Home.xaml.cs
+++ fp/Home.xaml.cs
@@ -16,6 +16,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.EntityFrameworkCore;
 using fp.Models;
+using Windows.Storage;
+using Microsoft.Data.Sqlite;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -51,6 +53,20 @@ namespace fp
             await Database.Context.SaveChangesAsync();
             
 
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List <Note> models;
+            models = await Database.Context.Notes.ToListAsync();
+            foreach(var item in models)
+            {
+                if(item.Title !=null || item.Content !=null)
+                {
+                    NoteCollection.Add(item);
+                }
+            }
+                // db.Users.FirstOrDefaultAsync(m => m.UserName == SignupBox1.Text);
         }
     }
 }
